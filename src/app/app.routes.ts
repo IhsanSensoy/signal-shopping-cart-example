4ae7f80881fee4049082703f7106dbe63ca8da4d
Ihsan Sensoy <ihsan.sensoy@gmx.de>
2025-10-25 13:32:20 +0200

    fix routing;

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,10 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => {
-      return import('./shopping-cart/components/shopping-cart-example/shooping-cart-example.component').then((m) => m.ShoppingCartExampleComponent);
+      return import('./shopping-cart/components/shopping-cart-example/shopping-cart-example.component').then((m) => m.ShoppingCartExampleComponent);
+    },
+      data: {
+      name: 'Shopping cart example',
     },
   },
   {
